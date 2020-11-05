11df763b5014120e7e6bdcf85091c244d0736cba
Tanvir <tanviramd32@gmail.com>
2020-11-06 01:23:21 +0600

    Initial commit

---
+++ DatingApp.Api/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace DatingApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController:ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public ValuesController(DataContext dataContext){
+            this._context=dataContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues(){
+            var values=await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetValue(int id){
+            var value=await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value){
+            _context.SaveChanges();
+        }
+
+        [HttpPost]
+        public void Put(int id,[FromBody] string value){
+
+        }
+    }
+}
---
+++ DatingApp.Api/Models/Value.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Api.Models
+{
+    public class Value
+    {
+        [Key]
+        public int Id{get;set;}
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address{get;set;}
+    }
+}
